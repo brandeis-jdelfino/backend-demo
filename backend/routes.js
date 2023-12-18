e92e7e94ff0cf983cc923d9356382cc454331b5f
Joe Delfino <jdelfino@gmail.com>
2023-12-18 15:41:11 +0000

    remove cors

--- backend/routes.js
+++ backend/routes.js
@@ -4,7 +4,6 @@ const router = Router();
 
 function hello(req, res) {
     const name = req.params?.name ?? "world";
-    res.setHeader('Access-Control-Allow-Origin', '*');
     res.send(`Hello ${name}!`);
 }
 
